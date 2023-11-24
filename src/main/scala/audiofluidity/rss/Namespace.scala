8d32ef40d0b94dea059ea6b866161dc669dbf886
Steve Waldman <swaldman@mchange.com>
2023-11-24 01:24:43 -0500

    Implement richer API for extracting Namespaces and managing conflicts.

--- src/main/scala/audiofluidity/rss/Namespace.scala
+++ src/main/scala/audiofluidity/rss/Namespace.scala
@@ -4,6 +4,9 @@ import scala.annotation.tailrec
 import scala.xml.{Elem,NamespaceBinding,TopScope}
 
 object Namespace:
+  final case class ExcludingConflicts( withUniquePrefixes : Set[Namespace], excluded : Map[String,Set[Namespace]] ):
+    lazy val excludedNamespaces = excluded.foldLeft( Set.empty[Namespace] )( (accum, nextTup) => accum ++ nextTup(1) )
+
   val RdfContent      = Namespace("content", "http://purl.org/rss/1.0/modules/content/")
   val ApplePodcast    = Namespace("itunes",  "http://www.itunes.com/dtds/podcast-1.0.dtd")
   val DublinCore      = Namespace("dc",      "http://purl.org/dc/elements/1.1/")
@@ -71,19 +74,40 @@ object Namespace:
   private def elemNamespacesDirect( accum : List[Namespace], elems : Seq[Elem] ) : List[Namespace] =
     elems.foldLeft( accum )( (soFar, next) => namespaces( soFar, next.map( _.scope ) ) )
 
-  def fromElemsRecursive( elems : Elem* ) : List[Namespace] = elemNamespacesRecursive( Nil, elems )
+  def fromElemsRecursive( elems : Elem* ) : Set[Namespace] = elemNamespacesRecursive( Nil, elems ).toSet
 
-  def fromElemRecursive( elem : Elem ) : List[Namespace] = fromElemsRecursive(Seq(elem)*)
+  def fromElemRecursive( elem : Elem ) : Set[Namespace] = fromElemsRecursive(Seq(elem)*).toSet
 
-  def fromElemsDirect( elems : Elem* ) : List[Namespace] = elemNamespacesDirect( Nil, elems )
+  def fromElemsDirect( elems : Elem* ) : Set[Namespace] = elemNamespacesDirect( Nil, elems ).toSet
 
-  def fromElemDirect( elem : Elem ) : List[Namespace] = fromElemsDirect(Seq(elem)*)
+  def fromElemDirect( elem : Elem ) : Set[Namespace] = fromElemsDirect(Seq(elem)*).toSet
+
+  def excludeConflicts( namespaces : Set[Namespace] ) : ExcludingConflicts =
+    val prefixGrouped = namespaces.groupBy( _.prefix )
+    prefixGrouped.foldLeft( ExcludingConflicts( Set.empty, Map.empty ) ): (accum, nextTup) =>
+      nextTup(1).size match
+        case 1 => accum.copy( withUniquePrefixes = accum.withUniquePrefixes ++ nextTup(1) )
+        case _ => accum.copy( excluded = (accum.excluded + nextTup).toMap )
+
+  def canonicalizeConflicts( excludingConflicts : ExcludingConflicts ) : ExcludingConflicts =
+    val canonicalizedConflicts = excludingConflicts.excluded.map( ( prefix, set ) => ( prefix, set.map( _.canonical ) ) )
+    val (resolved, remaining) = canonicalizedConflicts.partition( tup => tup(1).size == 1 )
+    val resolvedNamespaces = resolved.foldLeft( Set.empty[Namespace] )( (accum, nextTup ) => accum ++ nextTup(1) )
+    excludingConflicts.copy( withUniquePrefixes = excludingConflicts.withUniquePrefixes ++ resolvedNamespaces, excluded = remaining )
+
+  def canonicalizeConflicts( namespaces : Set[Namespace] ) : ExcludingConflicts =
+    val checkConflicts = excludeConflicts( namespaces )
+    if checkConflicts.excluded.nonEmpty then canonicalizeConflicts(checkConflicts) else checkConflicts
+
+  def canonicalizeAll( namespaces : Set[Namespace] ) : ExcludingConflicts =
+    excludeConflicts( namespaces.map( _.canonical ) )
 
   private def toBinding( parentScope : NamespaceBinding, list : List[Namespace] ) : NamespaceBinding =
     list match
       case head :: tail => toBinding(new NamespaceBinding(head.prefix, head.uri, parentScope), tail)
       case Nil          => parentScope
   def toBinding( namespaces : List[Namespace]) : NamespaceBinding = toBinding(TopScope, namespaces)
+  def toBinding( namespaces : Set[Namespace])  : NamespaceBinding = toBinding(TopScope, namespaces.toList)
 case class Namespace(prefix : String, uri : String):
   def canonical : Namespace = Namespace(prefix, Namespace.attemptCanonicalizeUri(uri))
 
---
+++ src/main/scala/audiofluidity/rss/exception.scala
@@ -0,0 +1,3 @@
+package audiofluidity.rss
+
+class AudiofluidityRssException( msg : String, cause : Throwable = null ) extends Exception( msg, cause )
