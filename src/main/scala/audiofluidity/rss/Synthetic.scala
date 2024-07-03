ead001a38e3bc7475f503d1c1be1f214cc62e19f
Steve Waldman <swaldman@mchange.com>
2024-07-03 02:35:14 -0400

    More work on parsers and generators for particular iffy:synthetic types.

--- src/main/scala/audiofluidity/rss/Synthetic.scala
+++ src/main/scala/audiofluidity/rss/Synthetic.scala
@@ -1,5 +1,6 @@
 package audiofluidity.rss
 
+import scala.collection.mutable
 import scala.xml.MetaData
 
 object Synthetic:
@@ -20,67 +21,78 @@ object Synthetic:
       case next :: rest =>
           _forwardExtrasExceptElements(elements, next :: accum)( rest )
 
-  def fromGenericSynthetic( synthetic : Element.Iffy.Synthetic ) : Synthetic =
-    val `type` =
-      val typeValue = synthetic.`type`.getOrElse(throw new UnparsableSynthetic( Seq("Missing type.") )()).value
-      Element.Iffy.Synthetic.KnownType.lenientParse( typeValue ) match
-        case Some( knownType ) => knownType
-        case None              => throw new UnparsableSynthetic( Nil, Some(typeValue) )()
-    `type` match
-      case Element.Iffy.Synthetic.KnownType.ItemUpdateFeed => fromItemUpdateFeedSynthetic( synthetic )
-      case Element.Iffy.Synthetic.KnownType.UpdateAnnouncement => fromUpdateAnnouncementSynthetic( synthetic )
-      case Element.Iffy.Synthetic.KnownType.UpdateCumulation => fromUpdateCumulationSynthetic( synthetic )
+  def fromGenericSynthetic( synthetic : Element.Iffy.Synthetic ) : ( Seq[String], Option[Synthetic] ) =
+    val warnings = Vector.newBuilder[String]
+    synthetic.`type` match // .getOrElse(throw new UnparsableSynthetic( Seq("Missing type.") )()).value
+      case None => ( Seq("Missing type."), None )
+      case Some( typeElem ) =>
+        Element.Iffy.Synthetic.KnownType.lenientParse( typeElem.value ) match
+          case Some( knownType ) =>
+            knownType match
+              case Element.Iffy.Synthetic.KnownType.ItemUpdateFeed => fromItemUpdateFeedSynthetic( warnings )( typeElem, synthetic )
+              case Element.Iffy.Synthetic.KnownType.UpdateAnnouncement => fromUpdateAnnouncementSynthetic( warnings )( typeElem, synthetic )
+              case Element.Iffy.Synthetic.KnownType.UpdateCumulation => fromUpdateCumulationSynthetic( warnings )( typeElem, synthetic )
+          case None => ( Seq("Missing type."), None )
 
-  def fromItemUpdateFeedSynthetic( synthetic : Element.Iffy.Synthetic ) : Synthetic =
-    //val warnings = Vector.newBuilder[String]
+  def fromItemUpdateFeedSynthetic( warnings : mutable.ReusableBuilder[String, Vector[String]] )( typeElement : Element.Iffy.Type, synthetic : Element.Iffy.Synthetic ) : ( Seq[String], Option[Synthetic] ) =
     val used     = Vector.newBuilder[Element[?]]
     val mbProvenance = synthetic.extraElements.collect{ case p : Element.Iffy.Provenance => p }.headOption
-    val itemLink =
-      synthetic.extraElements.collect{ case l : Element.Atom.Link => l }.headOption.getOrElse:
-        throw new UnparsableSynthetic( Seq("atom:link element required, not found.") )()
-    used ++= mbProvenance
-    used += itemLink
-    val parseWarnings = Nil
-    val forwardExtras = forwardExtrasExceptElements(used.result)( synthetic.reverseExtras )
-    val extraAttributs = synthetic.extraAttributes
-    ItemUpdateFeed( parseWarnings, forwardExtras, extraAttributs, mbProvenance, itemLink )
+    synthetic.extraElements.collect{ case l : Element.Atom.Link => l }.headOption match
+      case None => ( Seq("atom:link element required, not found."), None )
+      case Some( itemLink ) =>
+        used ++= mbProvenance
+        used += itemLink
+        val forwardExtras = forwardExtrasExceptElements(used.result)( synthetic.reverseExtras )
+        val extraAttributs = synthetic.extraAttributes
+        ( warnings.result, Some( ItemUpdateFeed( typeElement, forwardExtras, extraAttributs, mbProvenance, itemLink ) ) )
 
-  def fromUpdateAnnouncementSynthetic( synthetic : Element.Iffy.Synthetic ) : Synthetic =
+  def fromUpdateAnnouncementSynthetic( warnings : mutable.ReusableBuilder[String, Vector[String]] )( typeElement : Element.Iffy.Type, synthetic : Element.Iffy.Synthetic ) : ( Seq[String], Option[Synthetic] ) =
     val used = Vector.newBuilder[Element[?]]
     val mbProvenance = synthetic.extraElements.collect{ case p : Element.Iffy.Provenance => p }.headOption
-    val update =
-      synthetic.extraElements.collect{ case u : Element.Iffy.Update => u }.headOption.getOrElse:
-        throw new UnparsableSynthetic( Seq("iffy:update element required, not found.") )()
-    used ++= mbProvenance
-    used += update
-    val parseWarnings = Nil
-    val forwardExtras = forwardExtrasExceptElements(used.result)( synthetic.reverseExtras )
-    val extraAttributs = synthetic.extraAttributes
-    UpdateAnnouncement( parseWarnings, forwardExtras, extraAttributs, mbProvenance, update )
+    synthetic.extraElements.collect{ case u : Element.Iffy.Update => u }.headOption match
+        case None => ( Seq("iffy:update element required, not found."), None )
+        case Some( update ) =>
+          used ++= mbProvenance
+          used += update
+          val forwardExtras = forwardExtrasExceptElements(used.result)( synthetic.reverseExtras )
+          val extraAttributs = synthetic.extraAttributes
+          ( warnings.result, Some( UpdateAnnouncement( typeElement, forwardExtras, extraAttributs, mbProvenance, update ) ) )
 
-  def fromUpdateCumulationSynthetic( synthetic : Element.Iffy.Synthetic ) : Synthetic =
+  def fromUpdateCumulationSynthetic( warnings : mutable.ReusableBuilder[String, Vector[String]] )( typeElement : Element.Iffy.Type, synthetic : Element.Iffy.Synthetic ) :( Seq[String], Option[Synthetic] ) =
     val used = Vector.newBuilder[Element[?]]
     val mbProvenance = synthetic.extraElements.collect{ case p : Element.Iffy.Provenance => p }.headOption
-    val updateHistories = synthetic.extraElements.collect{ case uh : Element.Iffy.UpdateHistory => uh } // XXX: warn if empty!
-    used ++= mbProvenance
-    used ++= updateHistories
-    val parseWarnings = Nil
-    val forwardExtras = forwardExtrasExceptElements(used.result)( synthetic.reverseExtras )
-    val extraAttributs = synthetic.extraAttributes
-    UpdateCumulation( parseWarnings, forwardExtras, extraAttributs, mbProvenance, updateHistories )
+    val updateHistories = synthetic.extraElements.collect{ case uh : Element.Iffy.UpdateHistory => uh }
+    if updateHistories.isEmpty then
+      ( Seq("No iffy:update-history elements found in iffy:synthetic of type UpdateCumulation"), None )
+    else
+      used ++= mbProvenance
+      used ++= updateHistories
+      val forwardExtras = forwardExtrasExceptElements(used.result)( synthetic.reverseExtras )
+      val extraAttributs = synthetic.extraAttributes
+      ( warnings.result, Some( UpdateCumulation( typeElement, forwardExtras, extraAttributs, mbProvenance, updateHistories ) ) )
 
+  case class ItemUpdateFeed( `type` : Element.Iffy.Type, forwardExtras : List[Element.Extra], extraAttributes : MetaData, provenance : Option[Element.Iffy.Provenance], itemLink : Element.Atom.Link ) extends Synthetic.Derived:
+    def knownNontypeElements : List[Element[?]] = provenance.toList ::: itemLink :: Nil
 
-  case class ItemUpdateFeed( parseWarnings : Seq[String], forwardExtras : List[Element.Extra], extraAttributes : MetaData, provenance : Option[Element.Iffy.Provenance], itemLink : Element.Atom.Link ) extends Synthetic.Derived
-  case class UpdateAnnouncement( parseWarnings : Seq[String], forwardExtras : List[Element.Extra], extraAttributes : MetaData, provenance : Option[Element.Iffy.Provenance], update : Element.Iffy.Update ) extends Synthetic.Derived
-  case class UpdateCumulation( parseWarnings : Seq[String], forwardExtras : List[Element.Extra], extraAttributes : MetaData, provenance : Option[Element.Iffy.Provenance], updateHistories : Seq[Element.Iffy.UpdateHistory] ) extends Synthetic.Derived
+  case class UpdateAnnouncement( `type` : Element.Iffy.Type, forwardExtras : List[Element.Extra], extraAttributes : MetaData, provenance : Option[Element.Iffy.Provenance], update : Element.Iffy.Update ) extends Synthetic.Derived:
+    def knownNontypeElements : List[Element[?]] = provenance.toList ::: update :: Nil
+
+  case class UpdateCumulation( `type` : Element.Iffy.Type, forwardExtras : List[Element.Extra], extraAttributes : MetaData, provenance : Option[Element.Iffy.Provenance], updateHistories : Seq[Element.Iffy.UpdateHistory] ) extends Synthetic.Derived:
+    def knownNontypeElements : List[Element[?]] = provenance.toList ::: updateHistories.toList
 
   trait Derived extends Synthetic:
     def provenance : Option[Element.Iffy.Provenance]
 
 trait Synthetic:
-  def parseWarnings   : Seq[String]
+  def `type`          : Element.Iffy.Type
   def forwardExtras   : List[Element.Extra]
   def extraAttributes : MetaData
+
+  def knownNontypeElements : List[Element[?]]
+
+  def toElement       : Element.Iffy.Synthetic =
+    val reverseKnownNontypeExtras = knownNontypeElements.map( element => Element.Extra( element ) ).reverse
+    Element.Iffy.Synthetic(`type` = Some(`type`), namespaces = Nil, reverseExtras = forwardExtras.reverse ::: reverseKnownNontypeExtras, extraAttributes = extraAttributes, asLastParsed = None )
 
 
 
